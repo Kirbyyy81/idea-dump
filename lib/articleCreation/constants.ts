f47548aae85f82310c9fb0c97aaa0802f6cdc936
Ashley Chan Li Ling <ashley.chan@ytlcomms.my>
2026-04-15 19:16:50 +0800

    feat: implement new article creation page with tools

---
+++ lib/articleCreation/constants.ts
@@ -0,0 +1,10 @@
+export const IMAGE_NAME_PREFIX = 'taylors-article';
+
+export const IMAGE_SUFFIX_OPTIONS = [
+    'meta-image-1200x630',
+    'thumbnail-image',
+    'banner-image',
+    'hero-image',
+] as const;
+
+export const DEFAULT_IMAGE_SUFFIX = IMAGE_SUFFIX_OPTIONS[0];
